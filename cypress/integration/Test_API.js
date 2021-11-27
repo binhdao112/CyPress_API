48ed9e6a305bad0aba56f938b4b33a08873d58fd
binhdao112 <0932907271binh@gmail.com>
2021-11-27 13:42:13 +0700

    update second time

--- cypress/integration/Test_API.js
+++ cypress/integration/Test_API.js
@@ -1,5 +1,8 @@
 describe('Test API', () => {
     it('Request', () => {
-        
+        cy.server()
+        cy.log('fixtures:data2.json')
+        cy.route('/entries', 'fixture:data.json').as("GET")
+        cy.visit("/")
     });
 });
